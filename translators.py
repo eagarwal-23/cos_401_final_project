acbdc049d94f6f343148a620711faf68832c2c9f
eagarwal-23 <70918180+eagarwal-23@users.noreply.github.com>
2023-04-29 22:40:28 -0400

    she wears short skirts i wear t-shirts

--- translators.py
+++ translators.py
@@ -18,8 +18,75 @@ def main():
                     Et quand j’arriverai, je mettrai sur ta tombe
                     Un bouquet de houx vert et de bruyère en fleur."""
 
-    print("Poem translated by Google Translate:\n", translate_poem_google(sample_poem))
-    print("Poem translated by DeepL:\n", translate_poem_deepl(sample_poem))
+    sample_poem_2 = """Sous le pont Mirabeau coule la Seine
+                        Et nos amours
+                        Faut-il qu’il m’en souvienne
+                        La joie venait toujours après la peine
+
+                        Vienne la nuit sonne l’heure
+                        Les jours s’en vont je demeure
+
+                        Les mains dans les mains restons face à face
+                        Tandis que sous
+                        Le pont de nos bras passe
+                        Des éternels regards l’onde si lasse
+
+                        Vienne la nuit sonne l’heure
+                        Les jours s’en vont je demeure
+
+                        L’amour s’en va comme cette eau courante
+                        L’amour s’en va
+                        Comme la vie est lente
+                        Et comme l’Espérance est violente
+
+                        Vienne la nuit sonne l’heure
+                        Les jours s’en vont je demeure
+
+                        Passent les jours et passent les semaines
+                        Ni temps passé
+                        Ni les amours reviennent
+                        Sous le pont Mirabeau coule la Seine
+
+                        Vienne la nuit sonne l’heure
+                        Les jours s’en vont je demeure"""
+
+    poem_2_human_translation = """Under the Mirabeau Bridge
+                        there flows the Seine
+                        And our loves recall how then
+                        After each sorrow joy came back again
+
+                        Let night come on bells end the day
+                        The days go by me still I stay
+
+                        Hands joined and face to face let's stay just so
+                        While underneath
+                        The bridge of our arms shall go
+                        Weary of endless looks the river's flow
+
+                        Let night come on bells end the day
+                        The days go by me still I stay
+
+                        All love goes by as water to the sea
+                        All love goes by
+                        How slow life seems to me
+                        How violent the hope of love can be
+
+                        Let night come on bells end the day
+                        The days go by me still I stay
+
+                        The days the weeks pass by beyond our ken
+                        Neither time past
+                        Nor love comes back again
+                        Under the Mirabeau Bridge there flows the Seine
+
+                        Let night come on bells end the day
+                        The days go by me still I stay"""
+
+    print("Original poem:\n", sample_poem_2)
+    print("Poem translated by a human:\n", poem_2_human_translation)
+    print("Poem translated by Google Translate:\n", translate_poem_google(sample_poem_2))
+    print("Poem translated by DeepL:\n", translate_poem_deepl(sample_poem_2))
+    print("Poem translated by OpenAI:\n", translate_poem_openai(sample_poem_2))
 
 def translate_poem_google(poem, lang='en'):
     # translate poem from French to English
@@ -34,16 +101,16 @@ def translate_poem_deepl(poem, lang='EN-GB'):
     return poem_en.text
 
 def translate_poem_openai(poem, lang = 'English'):
-    openai.api_key = 'tfvQfDrGL6qp4cOveI5CT3BlbkFJ5AiIZCfxKfyUPqxoZfsL'
-    model = openai.Model.create(
+    openai.api_key = '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'
+    response = openai.Completion.create(
     engine="text-davinci-002",
-    prompt="Translate the following poem into, " + lang + ":\n\n" + poem + "\n\n1.",
+    prompt="Translate the following poem into, " + lang + ":\n" + poem + "\n.",
     temperature=0.5,
     max_tokens=1024,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0)
-
+    return response['choices'][0]['text'].strip()
 
 if __name__ == '__main__':
     main()
