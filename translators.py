1681add8a6b1023d62065aa4c7098ef2e0726dc6
eagarwal-23 <70918180+eagarwal-23@users.noreply.github.com>
2023-04-29 22:06:10 -0400

    smth

--- translators.py
+++ translators.py
@@ -1,5 +1,6 @@
 import googletrans
-import deepl 
+import deepl
+import openai
 
 def main():
     sample_poem = """Demain, dès l’aube, à l’heure où blanchit la campagne,
@@ -20,17 +21,29 @@ def main():
     print("Poem translated by Google Translate:\n", translate_poem_google(sample_poem))
     print("Poem translated by DeepL:\n", translate_poem_deepl(sample_poem))
 
-def translate_poem_google(poem_fr):
+def translate_poem_google(poem, lang='en'):
     # translate poem from French to English
     translator = googletrans.Translator()
-    poem_en = translator.translate(poem_fr, dest='en')
+    poem_en = translator.translate(poem, dest=lang)
     return poem_en.text
 
-def translate_poem_deepl(poem_fr):
+def translate_poem_deepl(poem, lang='EN-GB'):
     # translate poem from French to English
     translator = deepl.Translator('0c9c56f4-ef9d-0179-33a1-c1dde6e7ec9a:fx') 
-    poem_en = translator.translate_text(poem_fr, target_lang='EN-GB') 
+    poem_en = translator.translate_text(poem, target_lang=lang) 
     return poem_en.text
+
+def translate_poem_openai(poem, lang = 'English'):
+    openai.api_key = 'tfvQfDrGL6qp4cOveI5CT3BlbkFJ5AiIZCfxKfyUPqxoZfsL'
+    model = openai.Model.create(
+    engine="text-davinci-002",
+    prompt="Translate the following poem into, " + lang + ":\n\n" + poem + "\n\n1.",
+    temperature=0.5,
+    max_tokens=1024,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
+
 
 if __name__ == '__main__':
     main()
