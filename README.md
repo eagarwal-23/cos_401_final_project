cd24515774f6ffadfc8ab7c6ed041767e0ac7c3f
eagarwal-23 <70918180+eagarwal-23@users.noreply.github.com>
2023-04-30 22:32:13 -0400

    input poem as a text file from command line

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # COS 401 Final Project: Translating French Poetry
+
+Need two arguments to run translators, with the first being the poem to translate,
+and the second being a human translation of the same for comparison:
+
+`python translators.py poem1.txt poem1_en.txt`
--- translators.py
+++ translators.py
@@ -2,107 +2,35 @@ import config
 import googletrans
 import deepl
 import openai
+import sys
 
 def main():
-    sample_poem = """Demain, dès l’aube, à l’heure où blanchit la campagne,
-                    Je partirai. Vois-tu, je sais que tu m’attends.
-                    J’irai par la forêt, j’irai par la montagne.
-                    Je ne puis demeurer loin de toi plus longtemps.
+    with open(sys.argv[1], 'r') as f:
+        poem_fr = f.read()
+    with open(sys.argv[2], 'r') as f:
+        poem_en = f.read()
 
-                    Je marcherai les yeux fixés sur mes pensées,
-                    Sans rien voir au dehors, sans entendre aucun bruit,
-                    Seul, inconnu, le dos courbé, les mains croisées,
-                    Triste, et le jour pour moi sera comme la nuit.
-
-                    Je ne regarderai ni l’or du soir qui tombe,
-                    Ni les voiles au loin descendant vers Harfleur,
-                    Et quand j’arriverai, je mettrai sur ta tombe
-                    Un bouquet de houx vert et de bruyère en fleur."""
-
-    sample_poem_2 = """Sous le pont Mirabeau coule la Seine
-                        Et nos amours
-                        Faut-il qu’il m’en souvienne
-                        La joie venait toujours après la peine
-
-                        Vienne la nuit sonne l’heure
-                        Les jours s’en vont je demeure
-
-                        Les mains dans les mains restons face à face
-                        Tandis que sous
-                        Le pont de nos bras passe
-                        Des éternels regards l’onde si lasse
-
-                        Vienne la nuit sonne l’heure
-                        Les jours s’en vont je demeure
-
-                        L’amour s’en va comme cette eau courante
-                        L’amour s’en va
-                        Comme la vie est lente
-                        Et comme l’Espérance est violente
-
-                        Vienne la nuit sonne l’heure
-                        Les jours s’en vont je demeure
-
-                        Passent les jours et passent les semaines
-                        Ni temps passé
-                        Ni les amours reviennent
-                        Sous le pont Mirabeau coule la Seine
-
-                        Vienne la nuit sonne l’heure
-                        Les jours s’en vont je demeure"""
-
-    poem_2_human_translation = """Under the Mirabeau Bridge
-                        there flows the Seine
-                        And our loves recall how then
-                        After each sorrow joy came back again
-
-                        Let night come on bells end the day
-                        The days go by me still I stay
-
-                        Hands joined and face to face let's stay just so
-                        While underneath
-                        The bridge of our arms shall go
-                        Weary of endless looks the river's flow
-
-                        Let night come on bells end the day
-                        The days go by me still I stay
-
-                        All love goes by as water to the sea
-                        All love goes by
-                        How slow life seems to me
-                        How violent the hope of love can be
-
-                        Let night come on bells end the day
-                        The days go by me still I stay
-
-                        The days the weeks pass by beyond our ken
-                        Neither time past
-                        Nor love comes back again
-                        Under the Mirabeau Bridge there flows the Seine
-
-                        Let night come on bells end the day
-                        The days go by me still I stay"""
-
-    print("Original poem:\n", sample_poem_2)
-    print("Poem translated by a human:\n", poem_2_human_translation)
-    print("Poem translated by Google Translate:\n", translate_poem_google(sample_poem_2))
-    print("Poem translated by DeepL:\n", translate_poem_deepl(sample_poem_2))
-    print("Poem translated by OpenAI:\n", translate_poem_openai(sample_poem_2))
+    print("Original poem:\n", poem_fr, "\n")
+    print("Poem translated by a human:\n", poem_en, "\n")
+    print("Poem translated by Google Translate:\n", translate_poem_google(poem_fr), "\n")
+    print("Poem translated by DeepL:\n", translate_poem_deepl(poem_fr), "\n")
+    print("Poem translated by OpenAI:\n", translate_poem_openai(poem_fr), "\n")
 
 def translate_poem_google(poem, lang='en'):
-    # translate poem from French to English
+    # translate poem from French to English with Google Translate API
     translator = googletrans.Translator()
     poem_en = translator.translate(poem, dest=lang)
     return poem_en.text
 
 def translate_poem_deepl(poem, lang='EN-GB'):
-    # translate poem from French to English
+    # translate poem from French to English with deepl API
     translator = deepl.Translator(config.api_key_deepl) 
     poem_en = translator.translate_text(poem, target_lang=lang) 
     return poem_en.text
 
 def translate_poem_openai(poem, lang = 'English'):
-    openai.api_key = config.api_key_deepl
+    # translate poem from French to English with OpenAI API
+    openai.api_key = config.api_key_openai
     response = openai.Completion.create(
     engine="text-davinci-002",
     prompt="Translate the following poem into, " + lang + ":\n" + poem + "\n.",
