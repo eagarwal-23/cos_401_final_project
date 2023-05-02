b8319c43db7eaa4d99a11e20da93a194860fec75
eagarwal-23 <70918180+eagarwal-23@users.noreply.github.com>
2023-05-02 19:43:21 -0400

    lost in your current like a priceless wine

--- Poem_Emotions.py
+++ Poem_Emotions.py
@@ -1,9 +1,20 @@
 class Poem_Emotions():
-    def __init__(self, lang, polarity, emotions_vector, emotions_dict):
+
+    def __init__(self, lang, poem, polarity, emotions_vector, emotions_dict):
         self.lang = lang
+        self.poem = poem
         self.poem_polarity = polarity
         self.emotions_vector = emotions_vector
         self.emotions_dict = emotions_dict
+    
+    def __str__(self):
+        emotions = ['joy', 'fear', 'sadness', 'anger', 'surprise', 'disgust']
+        str_emotions = "Poem:\n" + self.poem + "\n"
+        str_emotions += "Emotions Dictionary: " +  str(self.emotions_dict) + "\n"
+        str_emotions += "Universal Emotions Vector: " + str(emotions) + "\n"
+        str_emotions += "Emotions Vector: " + str(self.emotions_vector) + "\n"
+        str_emotions += "Polarity: " + str(self.poem_polarity)
+        return str_emotions
 
     def emotions_normalized(self):
         emotions_normalized = {}
@@ -11,3 +22,6 @@ class Poem_Emotions():
         for emotion, count in (self.emotions_dict).items():
             emotions_normalized[emotion] = count/num_emotions
         return emotions_normalized
+    
+    def calc_dist(self, emotions_other):
+        return sum((p-q)**2 for p, q in zip(self.emotions_vector, emotions_other.emotions_vector)) ** 0.5
--- emotions.py
+++ emotions.py
@@ -3,15 +3,48 @@ import numpy as np
 import spacy
 import string
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, MWETokenizer
+from nltk.tokenize import MWETokenizer
 import Poem_Emotions
+import sys
 
 # constants
-nlp_fr = spacy.load('fr_core_news_md')
+nlp_en = spacy.load('en_core_web_lg')
+nlp_fr = spacy.load('fr_core_news_lg')
 emotions = ['joy', 'fear', 'sadness', 'anger', 'surprise', 'disgust']
+punctuation = string.punctuation
 
 def main():
-    pass
+    with open(sys.argv[1], 'r') as f:
+        poem_fr = f.read()
+    with open(sys.argv[2], 'r') as f:
+        poem_en = f.read()
+    calc_emotional_similarity(poem_fr, poem_en)
+
+def calc_emotional_similarity(poem_fr, poem_en):
+    # read in and preprocess emotion lexicons
+    feel_df = pd.read_csv('FEEL.csv', sep = ";")
+    nrc_df = pd.read_csv('nrc.txt', sep="\t", header=None)
+    feel_df_preprocessed = fr_preprocess_emotions_df(feel_df)
+    nrc_df_proprocessed = en_preprocess_emotions_df(nrc_df)
+
+    # create tokenizer for french poem
+    tokenizer = MWETokenizer()
+    tokenizer_fr = fr_create_phrase_tokenizer(feel_df_preprocessed, tokenizer)
+
+    # preprocess poems
+    cleaned_poem_fr = fr_preprocess_poem(poem_fr, tokenizer_fr)
+    cleaned_poem_en = en_preprocess_poem(poem_en)
+
+    # get emotions for both poems
+    poem_fr_emotions = fr_poem_emotions(poem_fr, cleaned_poem_fr, feel_df_preprocessed)
+    poem_en_emotions = en_poem_emotions(poem_en, cleaned_poem_en, nrc_df_proprocessed)
+
+    print(poem_fr_emotions)
+    print(poem_en_emotions)
+    print(poem_fr_emotions.calc_dist(poem_en_emotions))
+
+    return [poem_fr_emotions.calc_dist(poem_en_emotions), poem_fr_emotions, poem_en_emotions]
 
 def fr_preprocess_emotions_df(feel_df):
     # preprocess feel df, change polarity to numerical values
@@ -22,7 +55,10 @@ def fr_preprocess_emotions_df(feel_df):
     feel_df_multiple = feel_df[(feel_df['word'].str.split().apply(len)) > 1]
     feel_df_single = feel_df[(feel_df['word'].str.split().apply(len)) == 1]
     feel_df_single['word'] = feel_df_single['word'].apply(lambda x: nlp_fr(x)[0].lemma_)
-    return pd.concat(feel_df_single, feel_df_multiple)
+    
+    feel_df = pd.concat([feel_df_single, feel_df_multiple])
+    feel_df = feel_df.drop(['id'], axis = 1)
+    return feel_df
 
 def fr_create_phrase_tokenizer(feel_df, mwe_tokenizer):
     feel_df_multiple = feel_df[(feel_df['word'].str.split().apply(len)) > 1]
@@ -37,74 +73,115 @@ def fr_preprocess_poem(poem_fr, mwe_tokenizer):
     poem_fr_tokens_no_phrase = word_tokenize(poem_fr, language = 'french')
 
     # remove punctuation
-    punctuation = string.punctuation
     poem_fr_tokens_no_punc = [token for token in poem_fr_tokens_no_phrase if token not in punctuation]
 
     # lemmatize each token if single token else keep as is
     poem_fr_lemmatized = [nlp_fr(token)[0].lemma_ if "_" not in token else token for token in poem_fr_tokens_no_punc]
 
     # remove stopwords
-    stopwords = stopwords.words('french')
-    poem_fr_lemmatized_no_stopwords = [token for token in poem_fr_lemmatized if token not in stopwords]
-    
+    stopwords_fr = stopwords.words('french')
+    poem_fr_lemmatized_no_stopwords = [token for token in poem_fr_lemmatized if token not in stopwords_fr]
     # tokenize poem accounting for phrases
-    poem_fr_tokens_phrasal = mwe_tokenizer.tokenize(" ".join(poem_fr_lemmatized_no_stopwords))
+    poem_fr_tokens_phrasal = mwe_tokenizer.tokenize(poem_fr_lemmatized_no_stopwords)
 
     return poem_fr_tokens_phrasal
 
-def fr_poem_emotions(cleaned_poem_fr, feel_df):
+def fr_poem_emotions(poem_fr, cleaned_poem_fr, feel_df):
     # given a poem in french, return the emotions of the poem
     poem_emotions_vector = [0 for elem in range(len(emotions))]
     emotions_dict = {}
     poem_polarity = 0
-
     for token in cleaned_poem_fr:
-        token_row = (feel_df[feel_df['word'] == token])
-        for column in token_row:
-            if column == 'id' or column == 'word':
-                pass
-            elif column == 'polarity':
-                # calculating poem polarity
-                poem_polarity += (token_row[column].values[0])
-            else:
-                # creating emotions vector
-                if token_row[column].values[0] == 1:
-                    index = emotions.index(column)
-                    poem_emotions[index] += 1
+        print(token)
+        token = token.lower()
+        if token in feel_df['word'].values:
+            token_row = (feel_df[feel_df['word'] == token])
+            for column in token_row:
+                if column == 'id' or column == 'word':
+                    pass
+                elif column == 'polarity':
+                    # calculating poem polarity
+                    poem_polarity += (token_row[column].values[0])
+                else:
+                    # creating emotions vector
+                    if token_row[column].values[0] == 1:
+                        index = emotions.index(column)
+                        poem_emotions_vector[index] += 1
 
-                    # creating emotions dictionary
-                    if column in emotions_dict:
-                        emotions_dict[column] += 1
-                    else:
-                        emotions_dict[column] = 1
-    poem_emotions = Poem_Emotions('FR', poem_polarity, poem_emotions_vector, poem_emotions)
+                        # creating emotions dictionary
+                        if column in emotions_dict:
+                            emotions_dict[column] += 1
+                        else:
+                            emotions_dict[column] = 1
+    poem_emotions = Poem_Emotions.Poem_Emotions('FR', poem_fr, poem_polarity, poem_emotions_vector, emotions_dict)
     return poem_emotions
 
-def en_preprocess_emotions_df(lrc_lexicon):
-    pass
+def en_preprocess_emotions_df(nrc_df):
+    # adding column names
+    nrc_df.columns = ['word', 'emotion', 'association']
+
+    # changing orientation of df
+    nrc_df = nrc_df.pivot(index='word', columns='emotion', values='association')
+    nrc_df['word'] = nrc_df.index
+
+    # add column for polarity
+    conditions = [(nrc_df['positive'] == 1), (nrc_df['negative'] == 1)]
+    polarities = [1, -1]
+    nrc_df['polarity'] = np.select(conditions, polarities)
+    nrc_df = nrc_df.drop(['positive', 'negative',], axis = 1)
+
+    # drop columns not in french lexicon
+    nrc_df = nrc_df.drop(['anticipation', 'trust',], axis = 1)
+
+    nrc_df.dropna(inplace = True)
+
+    return nrc_df
+
+def en_preprocess_poem(poem_en):
+    # tokenize poem
+    poem_en_tokens_no_phrase = word_tokenize(poem_en, language = 'english')
+
+    # remove punctuation
+    poem_en_tokens_no_punc = [token for token in poem_en_tokens_no_phrase if token not in punctuation]
+
+    # lemmatize each token if single token else keep as is
+    poem_en_lemmatized = [nlp_en(token)[0].lemma_ for token in poem_en_tokens_no_punc]
+
+    # remove stopwords
+    stopwords_en = stopwords.words('english')
+    poem_en_lemmatized_no_stopwords = [token for token in poem_en_lemmatized if token not in stopwords_en]
+    
+    return poem_en_lemmatized_no_stopwords
+
+def en_poem_emotions(poem_en, cleaned_poem_en, nrc_df):
+    poem_emotions_vector = [0 for elem in range(len(emotions))]
+    emotions_dict = {}
+    poem_polarity = 0
+
+    for token in cleaned_poem_en:
+        token = token.lower()
+        if token in nrc_df['word'].values:
+            token_row = (nrc_df[nrc_df['word'] == token])
+            for column in token_row:
+                if column == 'word':
+                    pass
+                elif column == 'polarity':
+                    # calculating poem polarity
+                    poem_polarity += (token_row[column].values[0])
+                else:
+                    # creating emotions vector
+                    if token_row[column].values[0] == 1:
+                        index = emotions.index(column)
+                        poem_emotions_vector[index] += 1
+
+                        # creating emotions dictionary
+                        if column in emotions_dict:
+                            emotions_dict[column] += 1
+                        else:
+                            emotions_dict[column] = 1
+    
+    poem_emotions = Poem_Emotions.Poem_Emotions('EN', poem_en, poem_polarity, poem_emotions_vector, emotions_dict)
+    return poem_emotions
 
 if __name__ == '__main__':
     main()
-
-
-# # import pandas as pd
-# # from nrclex import NRCLex
-# # import sys
-# # from nltk.tokenize import word_tokenize
-# def main():
-#     # with open(sys.argv[1], 'r') as f:
-#     #     poem_fr = f.read()
-#     # with open(sys.argv[2], 'r') as f:
-#     #     poem_en = f.read()
-#     data = np.load('feel.npy', allow_pickle=True)
-#     print(data)
-#     # sentiment_poem_fr(poem_fr)
-
-# # def sentiment_poem_fr(poem_fr):
-# #     feel_df = pd.read_csv('FEEL.csv')
-# #     poem_fr_tokens = word_tokenize(poem_fr)
-# #     for token in poem_fr_tokens:
-# #         if token in feel_df["word"]:
-# #             print(feel_df["word"] == token)
-
-
