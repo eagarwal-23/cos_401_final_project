65b9d35121a0ffe866b5242ebfcf19b64a61f888
addeleh <addeleh@princeton.edu>
2023-05-04 01:27:57 -0400

    working ish
    emotions is taking forever on just 5 words so i'm scared to try a whole poem.

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+# TERMINAL COMMANDS:
+## pip install flash
+## pip install -r requirements.txt
+## python -m nltk.downloader punkt
+## python3 app.py
+
 from flask import Flask, render_template, request
+from translators import generate_translations
+from semantic import calculate_semantic_similarity
+from emotions import calc_emotional_similarity
 
 app = Flask(__name__)
 
@@ -11,21 +20,26 @@ def translate():
     og_poem = request.form['og_poem']
     human_trans = request.form['human_trans']
     # translation function
-    poem1 = og_poem
-    poem2 = human_trans
-    poem3 = og_poem
-    trans_poem = [poem1, poem2, poem3]
+    [google_trans, deepl_trans, openai_trans, human_trans] = generate_translations(og_poem, human_trans)
+    trans_poem = [human_trans, google_trans, deepl_trans, openai_trans]
 
     # semantic score for each translation
-    semantic = [len(poem1), len(poem2), len(poem3)]
+    semantic = [0,0,0,0]
+    for i in range(4):
+        semantic[i] = round(calculate_semantic_similarity(og_poem, trans_poem[i]), 5)
 
     # structural score for each translation
-    struct = [len(poem1)-1, len(poem2)-2, len(poem3)-3]
+    struct = [1,1,1,1]
 
     # emotional score for each translation
-    emotion = [len(poem1)+1, len(poem2)+2, len(poem3)+3]
+    emotion = [0,0,0,0]
+    for i in range(4):
+        emo = calc_emotional_similarity(og_poem, trans_poem[i])
+        emotion[i] = round(emo[0], 5)
 
-    return render_template('result.html', og_poem = og_poem, human_trans = human_trans, translation=trans_poem, semantic = semantic, struct = struct, emotion = emotion)
+    lexical = [0,0,0,0]
+
+    return render_template('result.html', og_poem = og_poem, translation=trans_poem, semantic = semantic, struct = struct, emotion = emotion, lexical = lexical)
 
 if __name__ == '__main__':
     app.run(debug=True)
